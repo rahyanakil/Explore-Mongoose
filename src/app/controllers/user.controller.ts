a96d8a1154a477d82bba0b559bc631a9ce0875b7
rahyanakil <rahyanakil89@gmail.com>
2025-07-09 01:17:25 +0600

    updated

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,104 +1,96 @@
-import express, {Request, Response } from "express";
-import { User } from "../models/user.model";
+import express, { Request, Response } from "express";
 import z from "zod";
-import bcrypt from "bcryptjs"
+import { User } from "../models/user.model";
 
+export const userRoutes = express.Router();
+const createUserZodSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  email: z.string(),
+  password: z.string(),
+  role: z.string().optional(),
+});
+//create user
+userRoutes.post("/create-user", async (req: Request, res: Response) => {
+  try {
+    //  const body =await createUserZodSchema.parseAsync(req.body);
 
-export const userRoutes =express.Router();
-const createUserZodSchema =z.object({
-    firstName:z.string(),
-    lastName: z.string(),
-    age:z.number(),
-    email:z.string(),
-    password:z.string(),
-    role:z.string().optional()
-})
-//create user 
-userRoutes.post("/create-user",async(req:Request,res:Response)=>{
-try{
+    // const password =await bcrypt.hash(body.password,10)
 
-//  const body =await createUserZodSchema.parseAsync(req.body);
+    //   body.password =password
 
-// const password =await bcrypt.hash(body.password,10)
+    //built in custom instance method
 
-//   body.password =password
+    //  const user =new User(body);
+    // const password =await user.hashpassword(body.password)
+    // console.log(password)
+    // user.password =password as string
+    //  await user.save()//instance method
+    const body = req.body;
+    //built in custom static methods
+    // const password = await User.hashpassword(body.password)
+    // console.log(password,"static");
+    // body.password=password
 
-//built in custom instance method 
+    const user = await User.create(body);
 
-//  const user =new User(body);
-// const password =await user.hashpassword(body.password)
-// console.log(password)
-// user.password =password as string
-//  await user.save()//instance method 
-const  body =req.body;
-//built in custom static methods
-const password = await User.hashpassword(body.password)
-console.log(password,"static");
-body.password=password
-
-const user =await User.create(body)
-
- res.status(201).json({
-    success:true,
-    message:"user created successfully",
-    user:user
- })
-}
-catch(error:any){
-    console.error(error)
-    res.status(400).json({
-        success:false,
-        message:error.message,
-        error
-    })
-}
-})
-
-//find all user 
-userRoutes.get("/",async(req:Request,res:Response)=>{
-    const user =await User.find()
     res.status(201).json({
-        success:true,
-        message:"All user fetched successfully",
-        user
+      success: true,
+      message: "user created successfully",
+      user: user,
+    });
+  } catch (error: any) {
+    console.error(error);
+    res.status(400).json({
+      success: false,
+      message: error.message,
+      error,
+    });
+  }
+});
 
-    })
-
-})
+//find all user
+userRoutes.get("/", async (req: Request, res: Response) => {
+  const user = await User.find();
+  res.status(201).json({
+    success: true,
+    message: "All user fetched successfully",
+    user,
+  });
+});
 
 //find a user
-userRoutes.get("/:id",async(req:Request,res:Response)=>{
-const user =await User.findOne()
-res.status(201).json({
-    success:true,
-    message:"The user is find successfully ",
-    user
-})
-})
-userRoutes.put("/updated-user/:id", async (req: Request, res: Response) =>{
-    const {id}= req.params;
-    console.log(id)
-    const updatedUserBody =req.body;
-    const updatedUser =await User.findByIdAndUpdate(id, updatedUserBody,{
-        new: true,
+userRoutes.get("/:id", async (req: Request, res: Response) => {
+  const user = await User.findOne();
+  res.status(201).json({
+    success: true,
+    message: "The user is find successfully ",
+    user,
+  });
+});
+userRoutes.put("/updated-user/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  console.log(id);
+  const updatedUserBody = req.body;
+  const updatedUser = await User.findByIdAndUpdate(id, updatedUserBody, {
+    new: true,
     runValidators: true,
-    });
-    res.status(201).json({
-        success:true,
-        message:"The user is updated Successfully",
-        updatedUser,
-    })
+  });
+  res.status(201).json({
+    success: true,
+    message: "The user is updated Successfully",
+    updatedUser,
+  });
+});
 
-})
-
-
-userRoutes.delete("/delete-user/:id",async(req:Request,res:Response)=>{
-    const {id} = req.params;
-    const deletedUser = await User.findByIdAndDelete(id);
-    res.status(201).json({
-        success:true,
-        message:"The user is deleted",
-        deletedUser
-    })
-
-})
+userRoutes.delete("/delete-user/:id", async (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  //   const deletedUser = await User.findByIdAndDelete(id);
+  const user = await User.findOneAndDelete({ _id: userId });
+  res.status(201).json({
+    success: true,
+    message: "The user is deleted",
+    user,
+  });
+});
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,72 +1,97 @@
-import { Model, model, Schema } from "mongoose";
-import { IUser, UserInstanceMethods, UserStaticMethods } from "../interfaces/user.interface";
+import bcrypt from "bcryptjs";
+import { model, Schema } from "mongoose";
 import validator from "validator";
-import bcrypt from "bcryptjs"
+import {
+  IUser,
+  UserInstanceMethods,
+  UserStaticMethods,
+} from "../interfaces/user.interface";
 
 // Define addressSchema or import it if defined elsewhere
 const addressSchema = new Schema({
-    street: { type: String, required: true },
-    city: { type: String, required: true },
-    country: { type: String, required: true }
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
 });
 
-const userSchema =new Schema<IUser,UserStaticMethods,UserInstanceMethods>({
-    firstName:{
-            type:String,
-            required:[true,"firstName keno dao nai"],
-            trim:true,
-            minlength:[3,"First Name must be at least 3 characters,got {VALUE}"],
-            maxlength:10
-        },
-        lastName:{
-            type:String,
-            required:true,
-            trim:true,
-            minlength:[3,"Name must be 3 characters"],
-            maxlength:10
-        },
-        age: {
-            type:Number,
-            required:true,
-            min:[18,"Age must be at least 18,got {VALUE}"],
-            age:60,
-        },
-    email:{ 
-        type:String,
-        required:true,
-        unique:[true,"Email common hoye giyeche"],
-
-        trim:true,
-        // validate:{
-        //     validator:function(value){
-        //         return /\d{3}-\d{3}-\d{4}/.test(value)
-        //     },
-        //     message:function (props){
-        //         return `Email ${props.value} is not valid email`
-        //     }
-        // }
-        validate:[validator.isEmail,"Invalid Email {VALUE}"]
+const userSchema = new Schema<IUser, UserStaticMethods, UserInstanceMethods>(
+  {
+    firstName: {
+      type: String,
+      required: [true, "firstName keno dao nai"],
+      trim: true,
+      minlength: [3, "First Name must be at least 3 characters,got {VALUE}"],
+      maxlength: 10,
     },
-    password:{
-        type:String,
-        required:true
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "Name must be 3 characters"],
+      maxlength: 10,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, "Age must be at least 18,got {VALUE}"],
+      age: 60,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "Email common hoye giyeche"],
+
+      trim: true,
+      // validate:{
+      //     validator:function(value){
+      //         return /\d{3}-\d{3}-\d{4}/.test(value)
+      //     },
+      //     message:function (props){
+      //         return `Email ${props.value} is not valid email`
+      //     }
+      // }
+      validate: [validator.isEmail, "Invalid Email {VALUE}"],
+    },
+    password: {
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     address: {
-        type: addressSchema,
-    }
-},
-{
+      type: addressSchema,
+    },
+  },
+  {
     versionKey: false,
     timestamps: true,
+  }
+);
+
+userSchema.method("hashPassword", async function (plainPassword: string) {
+  const password = await bcrypt.hash(plainPassword, 10);
+  return password;
+});
+// This method is used to compare the plain password with the hashed password using static method
+userSchema.static("hashpassword", async function (plainPassword: string) {
+  const password = await bcrypt.hash(plainPassword, 10);
+
+  return password;
 });
 
-userSchema.static("hashpassword",async function(plainPassword:string){
-const password =await bcrypt.hash(plainPassword,10)
+userSchema.pre("save", async function () {
+  //   console.log("inside pre save hook");
+  //   console.log(this);
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
-return password
-} )
-export const User = model<IUser,UserInstanceMethods>("User", userSchema);
+userSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    console.log(doc);
+    // await Note.deleteMany({ user: doc._id });
+  }
+});
+
+export const User = model<IUser, UserInstanceMethods>("User", userSchema);
